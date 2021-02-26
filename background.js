465045968319490d29599f8fa0b342fad4cd7ae6
ledleds <victorialedsom@gmail.com>
2021-02-26 21:50:42 +0000

    Show tick icon when clicked

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.executeScript(null, {file: "copyBranchScript.js"})
+});
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.browserAction.setIcon({
+    path: { 48: "images/clippy128-tick.png" },
+  });
+
+  setTimeout(() => { chrome.browserAction.setIcon({
+    path: { 48: "images/clippy128-orange.png" },
+  });
+ }, 2000);
 });
